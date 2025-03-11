bba01826038a810ee8bcd009ba944e7334a99f4b
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-12 01:02:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,8 +55,11 @@ tags= {
 }
 
 #creating s3 bucket
-resource "aws_s3control_bucket" "example" {
-  bucket     = "example"
-  outpost_id = data.aws_outposts_outpost.example.id
+data "aws_outposts_outpost" "example" {
+  arn = "arn:aws:outposts:us-east-1:123456789012:outpost/op-1234567890abcdef"
 }
 
+resource "aws_s3control_bucket" "example" {
+  bucket     = "example-bucket"
+  outpost_id = data.aws_outposts_outpost.example.id
+}
